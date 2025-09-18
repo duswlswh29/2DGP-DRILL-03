969c17a518fa9716d62b665896d47a0b714aa956
duswlswh29 <yeonjin1029@naver.com>
2025-09-18 12:28:09 +0900

    사각운동,삼각운동,원운동 함수 뼈대 완성

--- character_moves.py
+++ character_moves.py
@@ -2,7 +2,23 @@ from pico2d import *
 
 open_canvas()
 
+
+def move_rectangle():
+    pass
+
+
+def move_triangle():
+    pass
+
+
+def move_circle():
+    pass
+
+
 while True:
+    move_rectangle()
+    move_triangle()
+    move_circle()
     pass
 
 
