e8362462ed75eac828cb4bc1fa594affcdfc7880
duswlswh29 <yeonjin1029@naver.com>
2025-09-18 12:29:56 +0900

    전체 흐름 기본 검증

--- character_moves.py
+++ character_moves.py
@@ -4,14 +4,17 @@ open_canvas()
 
 
 def move_rectangle():
+    print("Move rectangle")
     pass
 
 
 def move_triangle():
+    print("Move triangle")
     pass
 
 
 def move_circle():
+    print("Move circle")
     pass
 
 
