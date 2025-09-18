f97fdaf7a073c45fdea0ab9b5ee209548f098d49
duswlswh29 <yeonjin1029@naver.com>
2025-09-18 12:44:54 +0900

    원운동 구현완료

--- character_moves.py
+++ character_moves.py
@@ -7,19 +7,25 @@ grass=load_image('grass.png')
 
 def move_rectangle():
     print("Move rectangle")
+    clear_canvas_now()
     pass
 
 
 def move_triangle():
     print("Move triangle")
+    clear_canvas_now()
     pass
 
 
 def move_circle():
     print("Move circle")
-    clear_canvas_now()
-    boy.draw_now(400,300)
-    delay(0.1)
+    r=200
+    for deg in range(0, 360):
+        x=r*math.cos(math.radians(deg))+400
+        y=r*math.sin(math.radians(deg))+300
+        clear_canvas_now()
+        boy.draw_now(x,y)
+        delay(0.1)
 
     pass
 
@@ -28,7 +34,7 @@ while True:
     move_rectangle()
     move_triangle()
     move_circle()
-
+    break
     pass
 
 
